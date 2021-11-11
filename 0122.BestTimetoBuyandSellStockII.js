f7d27874a93cd73b319d167e151a9aaa312eef17
Yunqi7 <63959277+Yunqi7@users.noreply.github.com>
2021-11-11 17:34:33 +0800

    Update 0122.BestTimetoBuyandSellStockII.js
    动态规划

--- 0122.BestTimetoBuyandSellStockII.js
+++ 0122.BestTimetoBuyandSellStockII.js
@@ -22,3 +22,19 @@ var maxProfit = function(prices) {
         return profit
     }
 };
+
+//动态规划
+var maxProfit = function(prices) {
+    if(prices.length===0){
+        return 0
+    }
+
+    let profit=0
+    for(let i=0;i<prices.length-1;i++){
+        if(prices[i+1]>prices[i]){
+            profit+=prices[i+1]-prices[i]
+        }
+    }
+    
+    return profit
+};
